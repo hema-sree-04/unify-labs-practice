a5bbabae3c2735dd7002dd980a66a2ea7b6d6f1b
Konjarla Hema Sree <hema24sree0407@gmail.com>
2026-02-09 21:26:34 +0530

    🚀 Day 13 - index.html - 9:26:32 pm

---
+++ Day-13/script.js
@@ -0,0 +1,8 @@
+// Day 13 Logic Practice
+console.log('Lab Session 13 Started');
+
+const selectedDay = 13;
+document.getElementById('day').textContent = selectedDay;
+
+const display = document.getElementById('display');
+display.textContent = 'Logic Engine Online';
