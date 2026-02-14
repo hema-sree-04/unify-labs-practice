b09926ba91651c2aa1eadc5a1e546f50f2e1670a
Konjarla Hema Sree <hema24sree0407@gmail.com>
2026-02-14 20:03:42 +0530

    🚀 Day 20 - index.html - 8:03:38 pm

---
+++ Day-20/script.js
@@ -0,0 +1,12 @@
+console.log("Lab Session 18 Started");
+
+// Day number
+let selectedDay = 20;
+
+// Change heading
+const title = document.getElementById("title");
+title.innerHTML = `Lab <span>Session</span> ${selectedDay}`;
+
+// Change debug panel text
+const display = document.getElementById("display");
+display.innerText = "Logic Engine Online";
